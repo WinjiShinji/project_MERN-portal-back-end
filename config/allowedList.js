5185533072a9590ba44473689a7e0fe471ec2dbf
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-02-28 08:20:38 +0000

    Deployment: - Updated vercel.json

--- config/allowedList.js
+++ config/allowedList.js
@@ -1,11 +1,11 @@
 const allowedList = [
+  'https://project-mern-portal-back-end.vercel.app/',
   'http://localhost:3500',
   'https://localhost:3500',
   'http://localhost:3000',
   'https://localhost:3000',
   'https://www.google.co.uk',
   'https://www.google.com',
-  'https://project-mern-portal-back-end.vercel.app/'
 ]
 
 // @DEV: test URIs
